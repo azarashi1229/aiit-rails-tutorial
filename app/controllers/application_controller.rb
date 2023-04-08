54e9351a0b360832384e69688ef04932a38273dd
MasayaDake <noimpslmtbrk@gmail.com>
2023-04-08 15:12:22 +0900

    database.ymlを戻し、メッセージ変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   def hello
-    render html: "¡Hola, mundo!"
+    # render html: "¡Hola, mundo!"
+    render html: "hello, world!"
   end
   def goodbye
     render html: "goodbye, world!"
